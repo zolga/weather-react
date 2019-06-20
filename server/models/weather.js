5ad4eb27313d4cae91fa316ede8342e2190eafce
Olga Zelenko <ozgitdeploy@gmail.com>
2019-06-20 14:21:04 -0700

    Added backend code

---
+++ server/models/weather.js
@@ -0,0 +1,18 @@
+const request = require('request-promise');
+const API_KEY = '16785bf86ab51b00c2a945e6a44f3e44';
+
+class Weather {
+  static retrieveByCity (city, callback) {
+    request({
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+      json: true
+    }).then((res) => {
+      callback(res);
+    }).catch((err) => {
+      console.log(err);
+      callback({ error: 'Could not reach OpenWeatherMap API.' });
+    });
+  }
+}
+
+module.exports = Weather;
